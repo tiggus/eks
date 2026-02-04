f7448fa320103a1cf3e5d00d840922bb08ff6fff
tiggus <53035370+tiggus@users.noreply.github.com>
2026-02-04 18:52:23 +0000

    updated spaces

--- .github/scripts/terraform.sh
+++ .github/scripts/terraform.sh
@@ -8,7 +8,7 @@ fi
 echo "environment: $1"
 
 # extract summary line from plan - change path was /tmp/plan_${environment}
-sed -r 's/\x1B\[0-9;]*[mK]//g' /tmp/${1}.tfplan > /tmp/${1}_summary
+sed -r 's/\x1B\[[0-9;]*[mK]//g' /tmp/${1}.tfplan > /tmp/${1}_summary
 summary=$(grep "Plan:" /tmp/${1}_summary || true)
 
 # extract counts with lookahead assertions - github
@@ -17,7 +17,7 @@ change=$(echo $summary | grep -oP '(\d+)(?= to change)' || echo "0")
 destroy=$(echo $summary | grep -oP '(\d+)(?= to destroy)' || echo "0")
 
 # extract resource names - adjust patterns if output format changes
-add_resources=$(grep -E '^  #' /tmp/${1}_summary | grep 'will be created' | sed 's/# //; s/ will be created//' || true)
+add_resources=$(grep -E '^  # ' /tmp/${1}_summary | grep 'will be created' | sed 's/# //; s/ will be created//' || true)
 change_resources=$(grep -E '  #' /tmp/${1}_summary | grep 'will be updated' | sed 's/# //; s/ will be updated.*//' || true)
 destroy_resources=$(grep -E '  #' /tmp/${1}_summary | grep 'will be destroyed' | sed 's/# //; s/ will be destroyed//' || true)
 destroy_resources=$(grep -E '  #' /tmp/${1}_summary | grep 'must be replaced' | sed 's/# //; s/ must be replaced//' || true)
