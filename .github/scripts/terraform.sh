9704413a786e6b0ff44a78f45af031497b2e4880
tiggus <53035370+tiggus@users.noreply.github.com>
2026-02-04 18:02:46 +0000

    commented greps

--- .github/scripts/terraform.sh
+++ .github/scripts/terraform.sh
@@ -38,8 +38,8 @@ cat /tmp/${1}.tfplan
 # destroy_resources=$(grep -E '^  # ' /tmp/dev.tfplan | grep 'will be destroyed' | sed 's/# //; s/ will be destroyed//' || true)
 # destroy_resources=$(grep -E '^  # ' /tmp/dev.tfplan | grep 'must be replaced' | sed 's/# //; s/ will be replaced//' || true)
 # echo $add_resources
-grep -E '^  # ' /tmp/dev.tfplan 
-echo 'grep'
+grep -E '^  # ' /tmp/${1}.tfplan 
+echo "test"
 # grep -E '^  # ' /tmp/dev.tfplan | grep 'will be created' 
 # grep -E '^  # ' /tmp/dev.tfplan | grep 'will be created' | sed 's/# //; s/ will be created//'
 
@@ -51,59 +51,59 @@ echo 'grep'
 # destroy_resources=$(grep -E '  #' /tmp/dev.tfplan | grep 'must be replaced' | sed 's/  # //; s/ must be replaced//' || true)
 
 
-# format as html function 
-format_list_html() {
-    if [ -z "$1" ]; then 
-        echo "<p>none</p>"
-    else
-    echo "$1" | sed 's/^/<li>/' | sed 's/$/<\/li>/' | paste -sd " " -
-fi
-}
+# # format as html function 
+# format_list_html() {
+#     if [ -z "$1" ]; then 
+#         echo "<p>none</p>"
+#     else
+#     echo "$1" | sed 's/^/<li>/' | sed 's/$/<\/li>/' | paste -sd " " -
+# fi
+# }
 
-add_list_html=$(format_list_html "$add_resources")
-change_list_html=$(format_list_html "$change_resources")
-destroy_list_html=$(format_list_html "$destroy_resources")
+# add_list_html=$(format_list_html "$add_resources")
+# change_list_html=$(format_list_html "$change_resources")
+# destroy_list_html=$(format_list_html "$destroy_resources")
 
-# build html
-html_content=$(cat <<EOF
-<h2>terraform plan summary ${1}</h2>
-<h3>overview</h3>
-<table border="1" cellspacing="0" cellpadding="3" >
-  <thead>
-    <tr>
-      <th align="left">action</th>
-      <th align="left">count</th>
-      <th align="left" >resources</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td><strong>add</strong></td>
-      <td>${add}</td>
-      <td><ul>${add_list_html}</ul></td>
-    </tr>
-    <tr>
-      <td><strong>change</strong></td>
-      <td>${change}</td>
-      <td><ul>${change_list_html}</ul></td>
-    </tr>
-    <tr>
-      <td><strong>destroy</strong></td>
-      <td>${destroy}</td>
-      <td><ul>${destroy_list_html}</ul></td>
-    </tr>
-  </tbody>
-</table>
+# # build html
+# html_content=$(cat <<EOF
+# <h2>terraform plan summary ${1}</h2>
+# <h3>overview</h3>
+# <table border="1" cellspacing="0" cellpadding="3" >
+#   <thead>
+#     <tr>
+#       <th align="left">action</th>
+#       <th align="left">count</th>
+#       <th align="left" >resources</th>
+#     </tr>
+#   </thead>
+#   <tbody>
+#     <tr>
+#       <td><strong>add</strong></td>
+#       <td>${add}</td>
+#       <td><ul>${add_list_html}</ul></td>
+#     </tr>
+#     <tr>
+#       <td><strong>change</strong></td>
+#       <td>${change}</td>
+#       <td><ul>${change_list_html}</ul></td>
+#     </tr>
+#     <tr>
+#       <td><strong>destroy</strong></td>
+#       <td>${destroy}</td>
+#       <td><ul>${destroy_list_html}</ul></td>
+#     </tr>
+#   </tbody>
+# </table>
 
-<h4>note: review detailed output prior to approval</h4>
-EOF
-)
+# <h4>note: review detailed output prior to approval</h4>
+# EOF
+# )
 
-echo $html_content >> $GITHUB_STEP_SUMMARY
-# write summary to step summary file
-echo $html_content > terraform_summary.md
-# export summary to output
-echo "summary<<EOF" >> $GITHUB_OUTPUT
-echo "$html_content" >> $GITHUB_OUTPUT
-echo "EOF" >> $GITHUB_OUTPUT
+# echo $html_content >> $GITHUB_STEP_SUMMARY
+# # write summary to step summary file
+# echo $html_content > terraform_summary.md
+# # export summary to output
+# echo "summary<<EOF" >> $GITHUB_OUTPUT
+# echo "$html_content" >> $GITHUB_OUTPUT
+# echo "EOF" >> $GITHUB_OUTPUT
 
