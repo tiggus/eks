2b6ac5296b0ab84cf63e0e6866ff8b635307cb06
tiggus <53035370+tiggus@users.noreply.github.com>
2026-03-18 01:23:29 +0000

    eks

--- modules.tf
+++ modules.tf
@@ -5,3 +5,62 @@ module "aurora" {
 # module "aurora-two" {
 #   source = "./modules/aurora"
 # }
+
+module "aws-eks" {
+  account_id          = data.aws_caller_identity.current.account_id
+  authentication_mode = "API"
+  cluster_root        = var.cluster_root
+  create_cluster      = true
+  cluster_version     = "1.34"
+  # providers = {
+  #   aws = aws.eu-west-2
+  # }
+  source             = "./modules/aws-eks"
+  private_subnet_ids = module.aws-vpc.private_subnet_ids
+  #  depends_on         = [module.aws-vpc]
+
+  # eks_managed_node_groups = {
+  #   one = {
+  #     name = "node-group-1"
+
+  #     instance_types = ["t3.small"]
+
+  #     min_size     = 1
+  #     max_size     = 3
+  #     desired_size = 2
+  #   }
+
+  #   two = {
+  #     name = "node-group-2"
+
+  #     instance_types = ["t3.small"]
+
+  #     min_size     = 1
+  #     max_size     = 2
+  #     desired_size = 1
+  #   }
+  # }
+}
+
+
+
+module "aws-vpc" {
+  create_vpc             = true
+  create_public_subnets  = true
+  create_private_subnets = true
+  create_igw             = true
+  enable_dns_hostnames   = true
+  enable_nat_gateway     = true
+  enable_dns_support     = true
+  ip_cidr_range          = var.ip_cidr_range
+  ip_private_subnets     = var.ip_private_subnets
+  ip_public_subnets      = var.ip_public_subnets
+  manage_security_group  = true
+  manage_nacl            = true
+  manage_route_table     = true
+  # providers = {
+  #   aws = aws.eu-west-2
+  # }
+  source             = "./modules/aws-vpc"
+  single_nat_gateway = true
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,29 @@ variable "secure_bucket" {
   type    = string
   default = "secure-test"
 }
+
+variable "cluster_root" {
+  type    = string
+  default = "eks-cluster"
+}
+
+variable "vpc_root" {
+  type    = string
+  default = "eks-vpc"
+}
+
+variable "ip_cidr_range" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "ip_private_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  type    = set(string)
+}
+
+variable "ip_public_subnets" {
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  type    = set(string)
+}
+
